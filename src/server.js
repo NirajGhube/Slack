79665b696ba2d7bd3f8c9f99dd5829cebcecfc6d
root <ghubeniraj90@gmail.com>
2025-11-19 11:55:41 +0530

    your commit message

--- src/server.js
+++ src/server.js
@@ -1,18 +1,38 @@
 import express from 'express';
 import {ENV} from "./config/env.js";
 import {connectDb} from "./config/db.js";
+import Express from 'express';
+import {clerkMiddleware} from "@clerk/express";
+import {functions, inngest} from "./config/inngest.js";
+import {serve} from "inngest/express";
 
 
 const app = express();
+app.use(express.json()); //req.body
+app.use(clerkMiddleware()); //req.auth will be available inthe request object
 
 const PORT = process.env.PORT;
 
-
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.get ("/", (req,res) =>{
     res.send("Hello World! 123");
 });
 console.log("Mongo uri: ",ENV.MONGO_URI);
-app.listen(ENV.PORT,()=>{
-    console.log("MongoDB server started on port",ENV.PORT);
-    connectDb()
-});
+
+const startServer = async () => {
+    try {
+        await connectDb();
+        if(ENV.NODE_ENV !== "production") {
+            app.listen(ENV.PORT,()=>{
+                console.log("MongoDB server started on port",ENV.PORT);
+                //connectDb()
+            });
+        }
+    }catch (error){
+        console.error("error starting server:",error);
+        process.exit(1);
+    }
+};
+startServer();
+
+export default app;
