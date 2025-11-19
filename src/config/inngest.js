ecc544975b97a938a9f082d3b0f8e0585162801a
root <ghubeniraj90@gmail.com>
2025-11-19 13:46:12 +0530

    vercel.json added

--- src/config/inngest.js
+++ src/config/inngest.js
@@ -13,7 +13,7 @@ const syncUser = inngest.createFunction(
           const newUser = {
               clerk_id:id,
               email:email_addresses[0]?.email_address,
-              name:'${first_name || ""} ${last_name || ""}',
+              name:`${first_name || ""} ${last_name || ""}`,
               image:image_url,
           }
           await Usr.create(newUser)
